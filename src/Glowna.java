852555400fe5554d6267175d4d8715bd54185833
Kryszczyn99 <krzysiopijanowski@gmail.com>
2021-03-03 16:11:19 +0100

    nowa metoda policz

--- src/Glowna.java
+++ src/Glowna.java
@@ -1,7 +1,12 @@
 public class Glowna {
+    static int Policz(int a, int b)
+    {
+        return a+b;
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("Raz dwa: ");
+        System.out.println("Raz dwa: " +Policz(2,3));
     }
 
 }
